e94a64ffbeec88e47d8f21be6e40d1ecbaa5b211
Shaun Ellis <shaun@sdellis.com>
2016-01-12 09:51:50 -0500

    fixed manifest drag n drop

--- _site/js/prospero.js
+++ _site/js/prospero.js
@@ -82,7 +82,7 @@ $.widget("pul.prospero", {
 var book = $("<div></div>")
   .appendTo("body")
   .prospero({
-    endpoint: "http://tabula.space/collections/manifests/",
+    endpoint: "http://45.55.133.0:3000/collections/manifests/",
     manifestUri: "foo:bar",
     complete: function(event, data) {
       $("#notice").text("Complete!");
